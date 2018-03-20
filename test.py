4b278e9fa149ec72f7698777d2dfa1116f222615
Wes Ackerman <wesley.ackerman@gmail.com>
2018-03-20 12:48:29 -0600

    First commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import soundfile as sf
+import numpy as np
+
+data, sample_rate = sf.read("librispeech/dev-clean/84/121123/84-121123-0001.flac")
+print(data.shape)
+print(sample_rate)
+sf.write("test.flac", data, int(sample_rate*.5))
+
