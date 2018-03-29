aa537a45d446db89fd877a63c2f175b3beea058f
S. Jacob Powell <s.jacob.powell@gmail.com>
2018-03-29 13:46:23 -0600

    Added a little structure to recognizer_trainer.py

---
+++ recognizer_trainer.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import os
+import tqdm, trange
+
+
+class RecognizerTrainer:
+    def __init__(self):
+        self.model_directory = './model'
+        self.sess = tf.Session()
+        self.saver = tf.train.Saver()
+
+    def train_recognizer(self, load_existing_model=True):
+        if load_existing_model and os.path.exists(self.model_directory):
+            self.load_trained_model()
+        else:
+            self.sess.run(tf.global_variables_initializer())
+
+    def save_trained_model(self):
+        self.saver.save(self.sess, os.path.join(self.model_directory, 'model'))
+
+    def load_trained_model(self):
+        self.saver.restore(self.sess, tf.train.latest_checkpoint(self.model_directory))
+
+
+if __name__ == '__main__':
+    RecognizerTrainer().train_recognizer()
