f525e2ce6f3d32ef86adb22099af066cae63cc81
Wes Ackerman <wesley.ackerman@gmail.com>
2018-04-06 13:57:04 -0600

    Text generation files

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import soundfile as sf
 import numpy as np
 
 data, sample_rate = sf.read("librispeech/dev-clean/84/121123/84-121123-0001.flac")
+print(type(data))
 print(data.shape)
 print(sample_rate)
 sf.write("test.flac", data, int(sample_rate*.5))
